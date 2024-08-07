a385fc811b0cd1ac6c4d5aecbdc48c9a3e54aadd
Ignácio <69769487+v1ncer3@users.noreply.github.com>
2024-08-06 23:25:24 -0300

    Update Server.js

--- Backend/config/server/Server.js
+++ Backend/config/server/Server.js
@@ -2,7 +2,7 @@ import { app } from '../../src/presentation/routes/Routes.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT_SERVER_BACKEND = process.env.PORT_SERVER_BACKEND || 3000;
+const PORT_SERVER_BACKEND = process.env.PORT_SERVER_BACKEND || 3030;
 
 app.listen(PORT_SERVER_BACKEND, () =>{
     console.log(`Server on port ${PORT_SERVER_BACKEND}`);
